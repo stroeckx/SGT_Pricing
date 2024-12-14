a080e1741cffd34f40c31059c5983233a2a7486f
Stijn Stroeckx <stijnstroeckx@hotmail.com>
2024-12-14 19:15:06 +0000

    TOC updated for 11.0.5

--- SGTPricing.lua
+++ SGTPricing.lua
@@ -4,7 +4,7 @@ SGTPricing.L = LibStub("AceLocale-3.0"):GetLocale("SGTPricing");
 --Variables start
 SGTPricing.majorVersion = 1;
 SGTPricing.subVersion = 1;
-SGTPricing.minorVersion = 7;
+SGTPricing.minorVersion = 8;
 SGTPricing.tsmNameString = "TradeSkillMaster";
 SGTPricing.auctionatorNameString = "Auctionator";
 --Variables end
